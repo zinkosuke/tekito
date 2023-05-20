15e9c4e5c74299b825afa129c63793aaf0b3b5ac
zinkosuke <48650930+zinkosuke@users.noreply.github.com>
2023-05-20 10:07:29 +0900

    fix

--- digdag/local_init.sh
+++ digdag/local_init.sh
@@ -6,8 +6,8 @@ echo '{"test": "aaaaaaaaaaaaaa"}' > .secrets/mydag.json
 docker-compose exec \
     -e AWS_ACCESS_KEY_ID=root \
     -e AWS_SECRET_ACCESS_KEY=password \
-    web aws --endpoint-url http://minio:9000 \
+    scheduler aws --endpoint-url http://minio:9000 \
     s3api create-bucket --bucket digdag-log
 
-docker-compose exec web ./scripts/push_workflows.sh
-docker-compose exec web ./scripts/push_secrets.sh
+docker-compose exec scheduler ./scripts/push_workflows.sh
+docker-compose exec scheduler ./scripts/push_secrets.sh
