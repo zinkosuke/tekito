3f5a54b9f01ffa9d42b06531ca474f1fe45ac122
zinkosuke <48650930+zinkosuke@users.noreply.github.com>
2024-04-14 03:20:02 +0900

    reverse proxy

--- oauth-reverse-proxy/main.go
+++ oauth-reverse-proxy/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	"github.com/caarlos0/env/v10"
@@ -10,14 +12,19 @@ import (
 	"proxy/pkg/auth"
 )
 
+const PATH_PREFIX = "/hec4XUHvwm"
+
 type App struct {
 	OAuthClientId     string `env:"OAUTH_CLIENT_ID"`
 	OAuthClientSecret string `env:"OAUTH_CLIENT_SECRET"`
 	CsrfToken         string `env:"CSRF_TOKEN"`
+	Listen            string
 }
 
 func NewApp() (*App, error) {
-	app := &App{}
+	app := &App{
+		Listen: ":80",
+	}
 	if err := env.Parse(app); err != nil {
 		slog.Error("NewApp: Application initialization failed.", err)
 		return nil, err
@@ -25,15 +32,19 @@ func NewApp() (*App, error) {
 	return app, nil
 }
 
-func (app *App) Index(w http.ResponseWriter, r *http.Request) {
+func (app *App) Healthcheck(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("pong"))
+}
+
+func (app *App) Redirect(w http.ResponseWriter, r *http.Request) {
 	// TODO ALB配下でhttps判定
-	redirectURL := "http://" + r.Host + "/callback"
+	redirectURL := "http://" + r.Host + PATH_PREFIX + "/callback"
 	oa := auth.NewGoogleOAuth(redirectURL, app.OAuthClientId, app.OAuthClientSecret)
 	http.Redirect(w, r, oa.GetAuthCodeURL(app.CsrfToken), http.StatusFound)
 }
 
 func (app *App) Callback(w http.ResponseWriter, r *http.Request) {
-	redirectURL := "http://" + r.Host + "/callback"
+	redirectURL := "http://" + r.Host + PATH_PREFIX + "/callback"
 	oa := auth.NewGoogleOAuth(redirectURL, app.OAuthClientId, app.OAuthClientSecret)
 
 	state := r.URL.Query().Get("state")
@@ -69,8 +80,8 @@ func (app *App) Callback(w http.ResponseWriter, r *http.Request) {
 	})
 	slog.Info("TODO expire", "x", token.Expiry)
 
-    // TODO IndexのURL覚えといてそっちに飛ばす
-	http.Redirect(w, r, "/success", http.StatusFound)
+	// TODO IndexのURL覚えといてそっちに飛ばす
+	http.Redirect(w, r, PATH_PREFIX+"/success", http.StatusFound)
 }
 
 func (app *App) Success(w http.ResponseWriter, r *http.Request) {
@@ -86,20 +97,40 @@ func (app *App) Success(w http.ResponseWriter, r *http.Request) {
 func main() {
 	app, err := NewApp()
 	if err != nil {
-		return
+		panic(err)
 	}
 
-	listen := "0.0.0.0:9999"
 	logHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.LevelInfo,
 	})
 	slog.SetDefault(slog.New(logHandler))
 
-	slog.Info("Server stated", "listen", listen)
+	// ReverseProxy
+	rp := &httputil.ReverseProxy{
+		Rewrite: func(r *httputil.ProxyRequest) {
+			target, err := url.Parse("http://app1")
+			if err != nil {
+				panic(err)
+			}
+			r.SetURL(target)
+			r.Out.Host = r.In.Host // if desired
+		},
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Ben", "Rad")
+		rp.ServeHTTP(w, r)
+	})
 
-	http.HandleFunc("/", app.Index)
-	http.HandleFunc("/callback", app.Callback)
-	http.HandleFunc("/success", app.Success)
-	http.ListenAndServe(listen, nil)
+	// OAuth
+	http.HandleFunc(PATH_PREFIX+"/ping", app.Healthcheck)
+	http.HandleFunc(PATH_PREFIX+"/redirect", app.Redirect)
+	http.HandleFunc(PATH_PREFIX+"/success", app.Success)
+	http.HandleFunc(PATH_PREFIX+"/callback", app.Callback)
+
+	slog.Info("Server stated.", "listen", app.Listen)
+	err = http.ListenAndServe(app.Listen, nil)
+	if err != nil {
+		panic(err)
+	}
 }
